4d02db5367cc0413a710e74a18d5ed479c48e1c3
Alex Blad <ablad@DHICorp.com>
2025-12-08 14:57:18 -0700

    Improved part 2 over earlier brute force method.

--- Aoc2025/Day1/SecretEntrance.py
+++ Aoc2025/Day1/SecretEntrance.py
@@ -25,16 +25,27 @@ print(zeros)
 
 # Part 2 Find the number of times the dials lands on or passes 0
 zeros = 0
-dial = 50
+current_position = 50
 
 for line in input:
     direction, distance = line[0], int(line[1:])
-    for num in range (distance):
-        if direction == "L":
-            dial = dial_positions[(dial - 1)%100]
-        if direction == "R":
-            dial = dial_positions[(dial + 1)%100]
-        if dial_positions[dial] == 0:
-            zeros += 1
+    
+    if direction == "L":
+        step_to_first_zero = current_position
+        if current_position == 0:
+            zeros += distance//100
+
+        elif distance >= step_to_first_zero:
+            zeros += 1 + (distance - step_to_first_zero)//100
+        current_position = (current_position - distance)%100
+
+    elif direction == "R":
+        step_to_first_zero = 100 - current_position
+        if current_position == 0:
+            zeros += distance//100    
+        
+        elif distance >= step_to_first_zero:
+            zeros += 1 + (distance - step_to_first_zero)//100
+        current_position = (current_position + distance)%100
 
 print(zeros)
